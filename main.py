3411980d4f0bd87ea673f76d228aae69bbb6a5d8
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2024-12-19 19:16:34 +0100

    Add data sources

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 import pandas as pd
+import os
+
+GSHEETS_OFFERS = os.environ.get('GSHEETS_OFFERS')
+GSHEETS_DESCRIPTIONS = os.environ.get('GSHEETS_DESCRIPTIONS')
+SHOPER_OFFERS_FILE_PATH = os.environ.get('SHOPER_OFFERS_FILE_PATH')
