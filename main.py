147285bb57e9ca403f2715ef3f070b399ac840ea
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2024-12-26 22:47:47 +0100

    Load a few columns from Shoper offers

--- main.py
+++ main.py
@@ -6,10 +6,23 @@ GSHEETS_OFFERS = os.environ.get('GSHEETS_OFFERS')
 GSHEETS_DESCRIPTIONS = os.environ.get('GSHEETS_DESCRIPTIONS')
 SHOPER_OFFERS_FILE_PATH = os.environ.get('SHOPER_OFFERS_FILE_PATH')
 
+shoper_columns_to_keep = ['product_code', 
+                          'images 1',
+                          'images 2',
+                          'images 3',
+                          'images 4',
+                          'images 5',
+                          'images 6',
+                          'images 7',
+                          'images 8',
+                          'images 9',
+                          'images 10']
+
 offers_to_create = pd.read_csv(GSHEETS_OFFERS)
 product_descriptions = pd.read_csv(GSHEETS_DESCRIPTIONS)
-shoper_offers = pd.read_csv(SHOPER_OFFERS_FILE_PATH, sep=';')
+shoper_offers = pd.read_csv(SHOPER_OFFERS_FILE_PATH, sep=';', usecols=shoper_columns_to_keep)
 
-functions.generate_css_file()
 
 print(shoper_offers)
+
+functions.generate_css_file()
